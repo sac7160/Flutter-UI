e1d41349392820b671964f9d525f4daed34a46d1
Chang Hyeon Park <sac7160@naver.com>
2022-01-21 00:58:30 +0900

    Add ProfilePage UI

---
+++ carrot_market_ui/lib/screens/my_carrot/my_carrot_screen.dart
@@ -0,0 +1,33 @@
+import 'package:carrot_market_ui/models/icon_menu.dart';
+import 'package:flutter/material.dart';
+import 'components/card_icon_menu.dart';
+import 'components/my_carrot_header.dart';
+
+class MyCarrotScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: const Text('나의 당근'),
+        actions: [
+          IconButton(icon: const Icon(Icons.settings), onPressed: () {}),
+        ],
+        bottom: const PreferredSize(
+            child: Divider(thickness: 0.5, height: 0.5, color: Colors.grey),
+            preferredSize: Size.fromHeight(0.5)),
+      ),
+      body: ListView(
+        children: [
+          MyCarrotHeader(),
+          const SizedBox(height: 8.0),
+          CardIconMenu(iconMenuList: iconMenu1),
+          const SizedBox(height: 8.0),
+          CardIconMenu(iconMenuList: iconMenu2),
+          const SizedBox(height: 8.0),
+          CardIconMenu(iconMenuList: iconMenu3),
+        ],
+      ),
+    );
+  }
+}
